58b0ddd84866b914c955d1a658df0c7c94eb89fc
@amanpreet <amanpreet@codalien.com>
2018-05-06 01:38:28 +0530

    added grace period

--- RangeDatepicker/DayRow.js
+++ RangeDatepicker/DayRow.js
@@ -1,7 +1,7 @@
 'use strict'
 import React from 'react';
 import {
-	View
+	View, Text, Image
 } from 'react-native';
 import Day from './Day';
 import moment from 'moment';
@@ -12,19 +12,30 @@ export default class DayRow extends React.Component {
 	}
 
 	shouldComponentUpdate(nextProps, nextState) {
-		if(JSON.stringify(nextProps.days) == JSON.stringify(this.props.days))
+		if (JSON.stringify(nextProps.days) == JSON.stringify(this.props.days))
 			return false;
 
 		return true;
 	}
 
 	render() {
+		const format = 'hh:mm:ss';
+		const currentTime = moment()
+		const previousDay = moment().subtract(1, 'days').format('YYYYMMDD');
+		const beforeTime = moment('00:00:00', format);
+		const afterTime = moment('04:00:00', format);
+		const gracePeriod = currentTime.isBetween(beforeTime, afterTime) ? true : false;
+		let showMoon = false; 
 		return (
-			<View style={{ marginBottom: 2, marginTop: 2, flexDirection: 'row'}}>
+			<View style={{ marginBottom: 2, marginTop: 2, flexDirection: 'row' }}>
 				{
+
+					
+
 					this.props.days.map((day, i) => {
+						showMoon = gracePeriod && (day.date === previousDay) ? true : false 
 						return (
-							<Day key={i} dayProps={this.props.dayProps} onSelectDate={this.props.onSelectDate} day={day}/>
+							<Day key={i} dayProps={this.props.dayProps} onSelectDate={this.props.onSelectDate} day={day} showMoon={showMoon} />
 						)
 					})
 				}
